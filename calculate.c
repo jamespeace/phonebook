76a7fb2216b07484fc26c55ca891f3cff277ab59
James Huang <james.happylove@gmail.com>
2017-05-02 16:28:59 +0800

    add the mempool data to out.txt

--- calculate.c
+++ calculate.c
@@ -63,8 +63,29 @@ int main(void)
 	}
 
 	fclose(fp);
-    fprintf(output, "append() %lf %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0, hash_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf %lf\n", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f / 100.0);
+
+	/* open mp.txt */
+	fp = fopen("mp.txt", "r");
+	if (!fp) {
+		printf("ERROR opening input file mp.txt\n");
+		exit(0);
+	}
+
+	double mp_sum_a = 0.0, mp_sum_f = 0.0, mp_a, mp_f;
+	for (i = 0; i < 100; i++) {
+		if (feof(fp)) {
+			printf("ERROR: You need 100 datum instead of %d\n", i);
+			printf("run 'make run' longer to get enough information\n\n");
+			exit(0);
+		}
+		fscanf(fp, "%s %s %lf %lf\n", append, find, &mp_a, &mp_f);
+		mp_sum_a += mp_a;
+		mp_sum_f += mp_f;
+	}
+
+	fclose(fp);
+    fprintf(output, "append() %lf %lf %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0, hash_sum_a / 100.0, mp_sum_a / 100.0);
+    fprintf(output, "findName() %lf %lf %lf %lf\n", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f / 100.0, mp_sum_f / 100.0);
     fclose(output);
     return 0;
 }
